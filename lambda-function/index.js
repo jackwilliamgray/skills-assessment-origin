11034a974fe3a3e75361388c6b0265c7ef45aa2f
Jack Gray <jack@zezamii.com>
2021-12-16 08:13:23 +1000

    Updated fetch request to retrieve coincap data

--- lambda-function/index.js
+++ lambda-function/index.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 let url = 'https://docs.aws.amazon.com/lambda/latest/dg/welcome.html';
 
 exports.handler = async function(event) {
-	const fetchResponse = await fetch(`https://api.github.com/users/${name}`);
+	const fetchResponse = await fetch(`https://api.coincap.io/v2/assets`);
 	const jsonData = await response.json();
 	const slicedData = jsonData.data.slice(0, data.data.length > 10 ? 10 : data.data.length);
 	const processedData = {
